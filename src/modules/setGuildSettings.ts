a521eeb3e4dcc6d2674d7dd0afad6ad784804fa1
LightBlueGamer <timjonsson03@hotmail.com>
2022-03-25 18:05:24 +0100

    Added another Reaction Role

--- src/modules/setGuildSettings.ts
+++ src/modules/setGuildSettings.ts
@@ -1,6 +1,8 @@
+/*
 import type { RoleResolvable, GuildMemberResolvable } from "discord.js";
 import { GuildSettings } from "../classes/GuildSettings";
 
 export function setGuildSettings(prefix: string, levelSettings: GuildSettings.LevelSettings, economySettings: GuildSettings.EconomySettings, administrators: Array<RoleResolvable | GuildMemberResolvable>, moderators: Array<RoleResolvable | GuildMemberResolvable>) {
     return new GuildSettings(prefix, levelSettings, economySettings, administrators, moderators);
 }
+*/
